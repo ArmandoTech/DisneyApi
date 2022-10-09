f9ccc7c152888247b98cfc7ed2cf6e9ba74ebf31
Armando <104610494+ArmandoTech@users.noreply.github.com>
2022-10-09 03:13:32 -0500

    advance/filter characters by queries

--- src/routes/characters/getController/getAllCharacters.controller.js
+++ src/routes/characters/getController/getAllCharacters.controller.js
@@ -2,7 +2,7 @@ import { getAllCharacters } from "../../../utils/characters/getAllCharacters.js"
 
 export const getAllCharactersController = async (req, res, next) => {
 	try {
-		const characters = await getAllCharacters();
+		const characters = await getAllCharacters(req.query);
 		res.status(200).json(characters);
 	} catch (error) {
 		next(error);
--- src/utils/characters/getAllCharacters.js
+++ src/utils/characters/getAllCharacters.js
@@ -1,6 +1,16 @@
 import { Character } from "../../dbRelations.js";
 
-export const getAllCharacters = async () => {
-	const characters = await Character.findAll();
-	return characters;
+export const getAllCharacters = async queries => {
+	const filters = addQueriesFind(queries);
+	return await Character.findAll({ where: { ...filters } });
+};
+
+const addQueriesFind = queries => {
+	const { name, age, weight, movies } = queries;
+	const queryFind = {};
+	if (name) queryFind.name = new RegExp(name, "i");
+	if (age) queryFind.age = age;
+	if (weight) queryFind.weight = weight;
+	if (movies) queryFind.movies = movies;
+	return queryFind;
 };
